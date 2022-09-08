3ce7b2e653757fa208d86c10be6ef10e9e34dbda
Chris Vernon <chrisrvernon@gmail.com>
2022-09-08 10:56:06 -0400

    new tests setup

---
+++ naturf/tests/test_config.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestConfig(unittest.TestCase):
+
+    def test_something(self):
+
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
