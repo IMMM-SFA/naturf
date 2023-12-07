962751ba5bb1560eb1b38a7c5eb86fe11f1ad3ed
erexer <13180883+erexer@users.noreply.github.com>
2023-12-07 13:36:38 -0800

    add timing

--- hamilton_run.py
+++ hamilton_run.py
@@ -1,4 +1,5 @@
 import naturf.nodes as nodes
+import time
 
 from hamilton import driver
 
@@ -10,9 +11,12 @@ if __name__ == "__main__":
     output_columns = ["write_binary", "write_index"]
     inputs = {"input_shapefile": path}
 
+    start_time = time.perf_counter()
+
     df = dr.execute(output_columns, inputs=inputs)
     dr.visualize_execution(
         output_columns, inputs=inputs, output_file_path="dag", render_kwargs={"format": "png"}
     )
 
-    print(df)
+    end_time = time.perf_counter()
+    print(f"Ran naturf using hamilton in {end_time - start_time:0.2f} seconds.")
