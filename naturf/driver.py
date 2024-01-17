71eec6f23917622039b1d468a45fefb1ef4805f7
Stefan Krawczyk <stefan@dagworks.io>
2024-01-17 10:55:00 -0800

    Adds ability to add adapters to Hamilton
    You now get a progress bar as things execute.
    Also if you have things set up, then you
    can also add the DAGWorks Tracking adapter
    to capture versions/runs/changes of the
    underlying Hamilton runs.

--- naturf/driver.py
+++ naturf/driver.py
@@ -1,10 +1,16 @@
+import os
 from typing import List, Union
 
 import pandas as pd
 from hamilton import driver
+from hamilton.plugins import h_tqdm
 
 import naturf.nodes as nodes
 import naturf.output as output
+
+DAGWORKS_API_KEY = os.environ.get("DAGWORKS_API_KEY")
+DAGWORKS_USERNAME = os.environ.get("DAGWORKS_USERNAME")
+DAGWORKS_PROJECT_ID = os.environ.get("DAGWORKS_PROJECT_ID")
 
 
 class Model:
@@ -15,8 +21,31 @@ class Model:
         # desired output parameters
         self.outputs = outputs
 
-        # instantiate driver with function definitions
-        self.dr = driver.Driver(self.inputs, nodes, output)
+        # instantiate any adapters we want
+        hamilton_adatpers = [
+            h_tqdm.ProgressBar("Naturf DAG"),
+        ]
+        if DAGWORKS_API_KEY and DAGWORKS_USERNAME and DAGWORKS_PROJECT_ID:
+            from dagworks import adapters
+
+            hamilton_adatpers.append(
+                adapters.DAGWorksTracker(
+                    project_id=int(DAGWORKS_PROJECT_ID),
+                    api_key=DAGWORKS_API_KEY,
+                    username=DAGWORKS_USERNAME,
+                    dag_name="naturf-dag",
+                    tags={"env": "dev", "status": "development", "version": "1"},
+                )
+            )
+
+        # instantiate driver with function definitions & adapters
+        self.dr = (
+            driver.Builder()
+            .with_config(self.inputs)
+            .with_modules(nodes, output)
+            .with_adapters(*hamilton_adatpers)
+            .build()
+        )
 
     def execute(self) -> pd.DataFrame:
         """Run the driver."""
@@ -26,10 +55,10 @@ class Model:
 
         return df
 
-    def graph(self, view: bool = True, output_file_path: Union[str, None] = None):
-        """Show the DAG."""
+    def graph(self, view: bool = True, output_file_path: Union[str, None] = None) -> object:
+        """Show the DAG. Return the graph object for the given inputs to execute."""
 
-        self.dr.visualize_execution(
+        return self.dr.visualize_execution(
             final_vars=self.outputs, output_file_path=output_file_path, render_kwargs={"view": view}
         )
 
